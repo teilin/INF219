8b36052a810cbaa0736fdf37a2197c75057e4103
Teis Lindemark <mail@teilin.net>
2012-02-26 15:33:20 +0100

    First commit

---
+++ Research/Evaluating_ontologies_mindmap.mm
@@ -0,0 +1,4 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1310507900841" ID="ID_241157925" MODIFIED="1310510329449" TEXT="Evaluating ontologies"/>
+</map>
